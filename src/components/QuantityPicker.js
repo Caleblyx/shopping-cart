f4319ecd6aca43d4399a2e300b0e180015b891e4
Caleb <caleblyx@outlook.com>
2022-11-16 18:03:37 +0800

    change site title and icon

---
+++ src/components/QuantityPicker.js
@@ -0,0 +1,10 @@
+const QuantityPicker = ({onChange, value=0}) => {
+    <div className="qty-picker">
+        hello
+        <div> - </div>
+        <div>{value}</div>
+        <div> + </div>
+    </div>
+}
+
+export default QuantityPicker;
